6a26e5dc53a0368ed2327a574f7d480ceaac6a42
tongxi_hu <tongxi_hu@jiliguala.com>
2025-08-19 18:24:18 +0800

    more asset

--- src/object/tea_pot.rs
+++ src/object/tea_pot.rs
@@ -6,12 +6,12 @@ use wgpu::util::DeviceExt;
 use crate::common::{Position, Rotation, Scale, Vertex};
 
 // position info
-pub const DEFAULT_SCALE: Scale = [100.0, 100.0, 100.0];
-pub const DEFAULT_ROTATION: Rotation = [0.0, 0.0, 0.0];
-pub const DEFAULT_POSITION: Position = [0., -1000.0, -1300.0];
+pub const DEFAULT_SCALE: Scale = [50.0, 50.0, 50.0];
+pub const DEFAULT_ROTATION: Rotation = [-90.0, 90.0, 0.0];
+pub const DEFAULT_POSITION: Position = [0., -300.0, -1300.0];
 
 pub fn generate_teapot_vertex() -> (u32, Vec<Vertex>) {
-    let content = fs::read_to_string("src/object/asset/body.obj").unwrap();
+    let content = fs::read_to_string("src/object/asset/head.obj").unwrap();
     let result = obj::parse(content).unwrap();
     let mut vertex = vec![];
     let count = result.objects[0].vertices.len();
