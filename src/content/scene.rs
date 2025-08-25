b3cdb5cd73e44e046888d8ef279b5386a44bce0b
tongxi_hu <tongxi_hu@jiliguala.com>
2025-08-25 12:23:27 +0800

    position

--- src/content/scene.rs
+++ src/content/scene.rs
@@ -85,7 +85,7 @@ pub fn generate_scene(size: PhysicalSize<u32>) -> Scene {
     let near: f32 = -1000.0;
     let far: f32 = -20000.0;
     // light
-    let light_position: [f32; 3] = [0.0, 500.0, -2000.0];
+    let light_position: [f32; 3] = [0.0, 1000.0, -800.0];
     let light_direction: [f32; 3] = [0.0, 0.0, -1.0];
     // eye
     let eye_position: [f32; 3] = [0.0, 0.0, 0.0];
